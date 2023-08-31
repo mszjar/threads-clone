c19dcab667faa0e302a3a965d595245f186d1552
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-08-31 16:50:12 +0200

    list of users added

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,14 +1,54 @@
 import React from 'react'
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import PostThread from '@/components/forms/PostThread';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import Image from 'next/image';
+import { profileTabs } from '@/constants';
+import ThreadsTab from '@/components/shared/ThreadsTab';
+import UserCard from '@/components/cards/UserCard';
 
-const Page = () => {
+async function Page() {
+  const user = await currentUser();
+  if(!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if(!userInfo?.onboarded) redirect('/onboarding');
+
+  // fetch users
+  const results = await fetchUsers({
+    userId: user.id,
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 25
+  })
+
   return (
     <section>
       <h1 className='head-text mb-10'>
         Search
       </h1>
+      <div className='mt-14 flex flex-col gap-9'>
+        {results.users.length === 0 ? (
+          <p className='no-result'>No users yet</p>
+        ) : (
+          <>
+            {results.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType='User'
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   )
 }
 
 export default Page
-
