b300307bafe60e909c29b7a197b8f2e77eabe9e3
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-08-31 17:34:11 +0200

    activity added

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -133,3 +133,31 @@ export async function fetchUsers({
     throw new Error(`Failed to fetch users: ${error.message}`)
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // find all threads created by the user
+    const userThreads = await Thread.find({ author: userId});
+
+    // collect all the child thread ids (replies) from the 'children'
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children)
+    }, [])
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }
+    }).populate({
+      path: 'author',
+      model: User,
+      select: 'name image _id'
+    })
+
+    return replies;
+
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user activity: ${error.message}`)
+  }
+}
